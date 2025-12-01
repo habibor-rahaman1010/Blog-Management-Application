4a049b43d2a4e7fb0c75a8e8589ca1928e0ddf55
habibor-rahaman1010 <habibor144369@gmail.com>
2025-12-02 00:27:24 +0600

    Blof Post Details done

--- BlogManagementApplication/Blog.Management.Web/Controllers/PublicBlogPostController.cs
+++ BlogManagementApplication/Blog.Management.Web/Controllers/PublicBlogPostController.cs
@@ -1,17 +1,19 @@
 ﻿using Blog.Management.Application.ApplicationDtos.BlogPostDtos;
 using Blog.Management.Application.ServiceInterfaces;
 using Microsoft.AspNetCore.Mvc;
-using System.Threading.Tasks;
 
 namespace Blog.Management.Web.Controllers
 {
     public class PublicBlogPostController : Controller
     {
         private readonly IBlogPostManagementService _blogPostManagementService;
+        private readonly ICategoryManagementService _categoryManagementService;
 
-        public PublicBlogPostController(IBlogPostManagementService blogPostManagementService)
+        public PublicBlogPostController(IBlogPostManagementService blogPostManagementService,
+            ICategoryManagementService categoryManagementService)
         {
             _blogPostManagementService = blogPostManagementService;
+            _categoryManagementService = categoryManagementService;
         }
 
         public async Task<IActionResult> BlogPostList(int pageIndex = 1, int pageSize = 12)
@@ -26,6 +28,22 @@ namespace Blog.Management.Web.Controllers
                 return View(new List<BlogPostDto>());
             }
 
+            catch (Exception ex)
+            {
+                throw new ApplicationException("Exception Occired: ", ex);
+            }
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> BlogDetails(Guid id)
+        {
+            try
+            {
+                var blogPost = await _blogPostManagementService.GetBlogPostByIdAsync(id);
+                var category = await _categoryManagementService.GetCategoryByIdAsync(blogPost.CategoryId);
+                blogPost.CategoryName = category.Name;
+                return View(blogPost);
+            }
             catch (Exception ex)
             {
                 throw new ApplicationException("Exception Occired: ", ex);
